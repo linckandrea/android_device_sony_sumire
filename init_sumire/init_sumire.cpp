112e426287ec62abcdcc87f20f3a8a0e5e7989e1
Andrea <linckandrea01@gmail.com>
2021-05-16 13:37:32 +0200

    bootup

--- init_sumire/init_sumire.cpp
+++ init_sumire/init_sumire.cpp
@@ -30,28 +30,33 @@
 #include <sys/types.h>
 
 #include "vendor_init.h"
+#include "property_service.h"
 #include "util.h"
 
-using android::init::import_kernel_cmdline;
-
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
-};
+using android::init::ImportKernelCmdline;
 
 void property_override(char const prop[], char const value[], bool add = true)
 {
-    prop_info *pi;
+    auto pi = (prop_info *) __system_property_find(prop);
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
+void property_override_triple(char const product_prop[], char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(product_prop, value);
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
 }
 
 static void import_kernel_nv(const std::string& key,
@@ -67,29 +72,28 @@ static void import_kernel_nv(const std::string& key,
             property_override("ro.telephony.default_network", "9,1");
             property_override("ro.semc.product.model", "E6683");
             property_override("ro.semc.product.name", "Xperia Z5 Dual");
-            for (const auto &source : ro_props_default_source_order) {
-	        set_ro_build_prop("source", "model", "E6683");
-	        set_ro_build_prop("source", "name", "sumire_dsds");
-	        set_ro_build_prop("source", "device", "sumire_dsds");
-	        set_ro_build_prop("source", "description", "sumire_dsds-user 7.1.1 N-MR1-KITAKAMI-170609-1025 1 dev-keys");
-	        set_ro_build_prop("source", "fingerprint", "Sony/sumire_dsds/sumire_dsds:7.1.1/N-MR1-KITAKAMI-170609-1025/1:user/dev-keys");
-            }
+            property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "E6683");
+            property_override_dual("ro.product.name", "ro.vendor.product.name", "sumire_dsds");
+            property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "sumire_dsds");
+            property_override("ro.build.description", "sumire_dsds-user 7.1.1 N-MR1-KITAKAMI-170609-1025 1 dev-keys");
+            property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "Sony/sumire_dsds/sumire_dsds:7.1.1/N-MR1-KITAKAMI-170609-1025/1:user/dev-keys");
         } else {
             property_override("ro.telephony.default_network", "9");
             property_override("ro.semc.product.model", "E6653");
             property_override("ro.semc.product.name", "Xperia Z5");
-            for (const auto &source : ro_props_default_source_order) {
-                property_override_dual("source", "model", "E6653");
-                property_override_dual("source", "name", "sumire");
-                property_override_dual("source", "device", "sumire");
-                property_override("source", "description",  "sumire-user 7.1.1 N-MR1-KITAKAMI-170609-1025 1 dev-keys");
-                property_override_dual("source", "fingerprint", "Sony/sumire/sumire:7.1.1/N-MR1-KITAKAMI-170609-1025/1:user/dev-keys");
-            }
+            property_override_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "E6653");
+            property_override_dual("ro.product.name", "ro.vendor.product.name", "sumire");
+            property_override_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "sumire");
+            property_override("ro.build.description", "sumire-user 7.1.1 N-MR1-KITAKAMI-170609-1025 1 dev-keys");
+            property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "Sony/sumire/sumire:7.1.1/N-MR1-KITAKAMI-170609-1025/1:user/dev-keys");
         }
     }
 }
 
 void vendor_load_properties()
 {
-    import_kernel_cmdline(0, import_kernel_nv);
+    ImportKernelCmdline([&](const std::string& key, const std::string& value) {
+        import_kernel_nv(key, value, false);
+    });
 }
+
