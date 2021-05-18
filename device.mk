9253dcc742dfcd2e312b3f0591ec66f9ba2c25d3
Andrea <linckandrea01@gmail.com>
2021-05-18 14:21:59 +0200

    sumire: build FP 2.1 HAL

--- device.mk
+++ device.mk
@@ -68,5 +68,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wlan/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/bcmdhd/bcmdhd.cal
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
