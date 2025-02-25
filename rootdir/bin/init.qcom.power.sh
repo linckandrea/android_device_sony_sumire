fa38deb418f2a6a96b14c0d3ada3d87ce553c855
linckandrea <linckandrea01@gmail.com>
2025-02-25 04:21:38 +0100

    sumire: power: make big cluster's 2 cores always online
    Change-Id: I8ef21863f36a4716231b7adc4549cffcb003ea9f

--- rootdir/bin/init.qcom.power.sh
+++ rootdir/bin/init.qcom.power.sh
@@ -85,7 +85,7 @@ write /sys/kernel/cpu_input_boost/enabled 1
 
 # Configure core_ctl module parameters
 write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 4
-write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1
+write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 2
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms  100
